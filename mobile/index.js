2f071849ee690edda729e0ed102e4bc459d345e1
Vonrroker <dayhamsoares@gmail.com>
2019-10-27 22:16:23 -0300

    igonore lifecicle depracated warnings

--- mobile/index.js
+++ mobile/index.js
@@ -1,8 +1,10 @@
 /**
  * @format
  */
+import { AppRegistry, YellowBox } from 'react-native';
 
-import { AppRegistry } from 'react-native';
+YellowBox.ignoreWarnings(['Warning: componentWillMount has been renamed'])
+
 import App from './src';
 import { name as appName } from './app.json';
 
